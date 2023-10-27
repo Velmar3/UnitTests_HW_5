fe057b9fe4f4017a21327158ac83cb9d58f01ad1
Your Name <you@example.com>
2023-10-27 12:33:03 +0300

    Add files

---
+++ unit-tests-5 (Код семинара)/src/seminars/six/Main.java
@@ -0,0 +1,17 @@
+package seminars.six;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Controller controller = new Controller();
+
+        int[] array1 = controller.createArray();
+        double averageArray1 = controller.averageArray(array1);
+
+        int[] array2 = controller.createArray();
+        double averageArray2 = controller.averageArray(array2);
+
+        controller.compareArray(averageArray1, averageArray2);
+    }
+}
---
+++ unit-tests-5 (Код семинара)/src/seminars/six/model/IList.java
@@ -0,0 +1,6 @@
+package seminars.six.model;
+
+public interface IList {
+    void compareArray(double resultAverage1, double resultAverage2);
+    double averageArray(int[] array);
+}
